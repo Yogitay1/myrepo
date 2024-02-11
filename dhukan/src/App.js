9c9265014f56d3aa95724cde271fe8135874c698
Yogitay1 <146335403+Yogitay1@users.noreply.github.com>
2024-02-11 12:39:17 +0530

    second commit

--- dhukan/src/App.js
+++ dhukan/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Slider from './Components/Home/Slider';
-import Header from './Components/Navbar/Header';
-
-
+//import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Slider from "./Components/Home/Slider";
+import Header from "./Components/Navbar/Header";
 
 function App() {
   return (
-    <Slider/>
+    <div>
+      
+      <div className="container">
+              <Header />
+      </div>
+
+    <Slider />
+    </div>
   );
 }
 
--- dhukan/src/Components/Navbar/Header.js
+++ dhukan/src/Components/Navbar/Header.js
@@ -1,9 +1,42 @@
 import React from 'react'
-import { FiAlignJustify } from "react-icons/fi";
+import './Header.css';
+import '../Assets/stalls.png'
+
+
+import {Link} from "react-router-dom";
+
+import { FiAlignJustify} from "react-icons/fi";
+import { CiSearch } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa6";
+import { CiShoppingCart } from "react-icons/ci";
+import { VscAccount } from "react-icons/vsc";
 
 const Header = () => {
   return (
-    <div><FiAlignJustify /></div>
+   // <div><FiAlignJustify /></div>
+   < div className='headernav'>
+   <div className="left-section">
+    <FiAlignJustify className='hamburger-menu'/> 
+      </div>
+
+ <div className="middle-section">
+      <img src='stalls.png'  className='stalls'></img>
+
+   <input className= "search-bar" type="text" placeholder="serach"/>
+   <button className="search-button">
+   <div>
+   <CiSearch  button className='search-icon'/>
+       </div>
+   </button>
+ </div>
+
+ <div className="right-section">
+ <FaHeart className="wishlist" />
+ <CiShoppingCart className="cart" />
+< VscAccount  className="current-user-picture"/> 
+ </div>
+
+</div>
   )
 }
 
